6f78c7780b215e42504cea98a71b7c86988d3636
aqlahmad <aqlahmad@hotmail.com>
2023-11-18 20:10:32 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000 
+
+
+app.get('/', (req, res) => {
+
+  fs.readFile('html\\index.html', 'utf-8', (err, html) => {
+
+    if (err) {
+      console.log(err)
+      res.status(500).send("server error")
+    }
+
+    res.send(html)
+
+  })
+})
+
+app.listen(port, () => {
+  
+  console.log(`Example app listening on port ${port}`)
+})
