56796192340c279d45a0872501fc72bc97471594
aqlahmad <146508844+aqlahmad@users.noreply.github.com>
2023-11-18 20:20:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
 
-  fs.readFile('html\\index.html', 'utf-8', (err, html) => {
+  fs.readFile('html/index.html', 'utf-8', (err, html) => {
 
     if (err) {
       console.log(err)
